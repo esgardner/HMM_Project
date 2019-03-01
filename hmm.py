18447e9c1d5ee20c5c7603f81af129f342b98532
eahorn2 <eahorn2@illinois.edu>
2019-03-01 12:41:48 -0800

    Further comments on hmm.py

--- hmm.py
+++ hmm.py
@@ -4,23 +4,42 @@ import sys, math, numpy, collections
 
 class HMM_Parser():
     def parse(self, hmm_filename):
-        # Variables for parsing results
+        ## Variables for parsing results
+        # Number of parsed initial lines
         parsed_init_num = 0
+
+        # Number of parsed transition lines
         parsed_trans_num = 0
+
+        # Number of parsed emission lines
         parsed_emiss_num = 0
+
+        # Initial state probabilities: Dictionary of state -> probability
         init_state_probs = collections.defaultdict(float)
+
+        # Transition probabilities: Dictionary of start state -> end state -> probability
         transition_probs = collections.defaultdict(lambda: collections.defaultdict(float))
+
+        # Set of states in HMM
         states = set()
+
+        # Emission probabilities: Dictionary of state -> emission -> probability
         emission_probs = collections.defaultdict(lambda: collections.defaultdict(float))
+
+        # Set of emitted items in HMM
         emissions = set()
+
+        # Dictionary of emissions -> set of states that emitted that item
         emission_to_states = collections.defaultdict(set)
 
+        # Open HMM file
         with open(hmm_filename, 'r') as infile:
             parse_state = None
+
+            # Iterate through HMM file
             for line in infile:
                 # Strip any trailing whitespace from line (including newlines)
                 line = line.rstrip()
-                split_line = line.split()
                 
                 # If we encounter an empty line, skip it
                 if line == "":
@@ -60,7 +79,7 @@ class HMM_Parser():
 
                     # Check this isn't a duplicate transition
                     if start_state in transition_probs and end_state in transition_probs[start_state]:
-                        print("warning: duplicated transition {} {}".format(start_state, end_state),file=sys.stderr)
+                        print("warning: duplicated transition {} {}".format(start_state, end_state))
                         sys.exit()
                     else:
                         transition_probs[start_state][end_state] = probability
@@ -79,7 +98,7 @@ class HMM_Parser():
 
                     # Check this isn't a duplicate emission
                     if state in emission_probs and emission in emission_probs[state]:
-                        print("warning: duplicated emission {} {}".format(state, emission), file=sys.stderr)
+                        print("warning: duplicated emission {} {}".format(state, emission))
                         sys.exit()
                     else:
                         emission_probs[state][emission] = probability
@@ -96,12 +115,12 @@ class HMM_Parser():
                     # Update count of parsed emission lines
                     parsed_emiss_num += 1
                 
-                # We should never hit this condition
+                # We should never hit this condition if our HMM is valid
                 else:
-                    print("Unexpected item encountered in parsing:{}".format(line), file=sys.stderr)
+                    print("Unexpected item encountered in parsing:{}".format(line))
                     sys.exit()
 
-
+        # Put all of the HMM information in a single dictionary
         return {
             "parsed_init_num": parsed_init_num,
             "parsed_trans_num": parsed_trans_num,
@@ -115,14 +134,18 @@ class HMM_Parser():
         }
         
 
+    ## Extract the last integer in a space separated string
+    # e.g. "State 2" -> 2
     def _get_last_int_in_line(self, line):
         try:
             return int(line.split()[-1])
         except:
-            print("Error: failed to parse line:{}".format(line), file=sys.stderr)
+            print("Error: failed to parse line:{}".format(line))
             sys.exit()
             return 0
 
+    # Extract information about initial states from a line
+    # "BOS 1.0" -> ("BOS", 1.0)
     def parse_init_line(self,line):
         # Split line on spaces
         split_line = line.split()
@@ -132,6 +155,8 @@ class HMM_Parser():
         prob = float(split_line[1])
         return (state, prob)
     
+    # Extract information about transitions from a line
+    # "BOS DT 0.5" -> ("BOS", "DT", 0.5)
     def parse_transition_line(self,line):
         # Split line on spaces
         split_line = line.split()
@@ -147,6 +172,8 @@ class HMM_Parser():
             sys.exit()
         return (start_state, end_state, prob)
     
+    # Extract information about emissions from a line
+    # "DT the 0.5" -> ("DT", "the", 0.5)
     def parse_emission_line(self,line):
         # Split line on spaces
         split_line = line.split()
@@ -168,6 +195,7 @@ class HMM():
     def __init__(self, hmm_filename):
         self.__load_hmm(filename)
     
+    # Load a hmm model file
     def __load_hmm(self, hmm_filename):
         hmm_data = HMM_Parser().parse(hmm_filename)
         
